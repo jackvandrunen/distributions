030ebf06438e7307fed5a13e3378c94661450a83
Jack VanDrunen <jack@jackv.co>
2018-05-18 10:29:46 -0700

    Moved to findtests build/test system

--- nakefile.nim
+++ nakefile.nim
@@ -1,67 +1,7 @@
 import nake
+import findtests
 import strformat
-import os
-import osproc
-import sets
-import strutils
 import sequtils
-
-
-## This is a small module which dynamically detects module dependencies, useful
-## for automatically recompiling and running unit tests based on which files
-## have been updated since the test harness was built last.
-## TODO: This should probably end up as its own library on Nimble
-
-
-proc joinPaths(path1, path2: string): string =
-  if path1[^1] == '/':
-    path1 & path2
-  else:
-    path1 & "/" & path2
-
-proc parentPath(path: string): string =
-  var i = path.len - 2  # ignore trailing slash
-  while path[i] != '/':
-    dec i
-  path[0..i]
-
-proc expandPath(filename, root: string): string =
-  if filename[0..1] == "./":
-    joinPaths(root, filename[2..^1])
-  elif filename[0..2] == "../":
-    joinPaths(parentPath(root), filename[3..^1])
-  elif filename[0] == '/':
-    filename
-  else:
-    joinPaths(root, filename)
-
-proc followDependencyTree(path: string, includes: ptr HashSet[string]) =
-  let imports = splitLines(execProcess("/bin/grep", @["import \\.", path], options={}))
-  let root = parentPath(path)
-  for i in 0..imports.len-1:
-    if imports[i].len > 0 and not includes[].contains(imports[i]):
-      let newInclude = expandPath(imports[i][7..^1] & ".nim", root)
-      includes[].incl(newInclude)
-      followDependencyTree(newInclude, includes)
-
-proc getRelativeImports(path: string): HashSet[string] =
-  result = initSet[string]()
-  followDependencyTree(path, addr result)
-
-proc includeTests(path: string, exclude: openarray[string]): seq[seq[string]] =
-  result = newSeq[seq[string]]()
-  let testsDir = expandFilename(path)
-  var toExclude = initSet[string](exclude.len)
-  for excludeFile in exclude:
-    toExclude.incl(expandPath(excludeFile, testsDir))
-  for testSrc in walkFiles(joinPaths(testsDir, "*.nim")):
-    if toExclude.contains(testSrc):
-      continue
-    result.add(concat(@[testSrc], toSeq(getRelativeImports(testSrc).items())))
-
-
-## This is the actual code for the nakefile
-
 
 const
   TESTS = "tests"
@@ -71,7 +11,7 @@ const
 
 proc buildAll(): seq[string] =
   result = newSeq[string]()
-  for testFiles in includeTests(TESTS, EXCLUDES):
+  for testFiles in findTests(TESTS, EXCLUDES):
     let target = testFiles[0][0..^5]
     if needsRefresh(target, testFiles):
       echo fmt"Building {target}..."
@@ -84,7 +24,7 @@ proc runTests(testsToRun: openarray[string]) =
     shell(target)
 
 proc clean() =
-  for testFiles in includeTests(TESTS, EXCLUDES):
+  for testFiles in findTests(TESTS, EXCLUDES):
     let target = testFiles[0][0..^5]
     echo fmt"Removing {target}..."
     removeFile(target)
@@ -95,6 +35,10 @@ task "test", "Run unit tests on all newly modified files":
   let testsToRun = buildAll()
   runTests(testsToRun)
   echo "\pAll tests updated!"
+
+task "retest", "Run all unit tests":
+  runTask("clean")
+  runTask("test")
 
 task "clean", "Delete all test binaries and cache":
   clean()
