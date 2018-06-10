d3ec6090bcd4ed2fcf8984f3f87e75b2a61ce6bb
Jack VanDrunen <jack@jackv.co>
2018-06-09 23:57:08 -0700

    Multiple oracles now supported

--- statistics/oracle.nim
+++ statistics/oracle.nim
@@ -4,22 +4,31 @@ const Discard = 3072
 
 var defaultState: Oracle
 
+proc initOracle*(seed: uint64): Oracle =
+  result = initARC4(cast[array[0..7, uint8]](seed))
+  for b in generate(result, Discard):
+    discard
+
 when not defined(nimscript):
   import times
-  let seed = uint64(epochTime() * 1_000_000_000)
-  defaultState = initARC4(cast[array[0..7, uint8]](seed))
-  for b in generate(defaultState, Discard):
-    discard
+
+  proc initOracle*(): Oracle =
+    initOracle(uint64(epochTime() * 1_000_000_000))
+
+  defaultState = initOracle()
 else:
   {.error: "oracle is not supported for the nimscript target".}
 
-proc rand*(): float =
+proc rand*(state: var Oracle): float =
   var bytes: array[0..7, uint8]
   var number: uint64
   var i = 0
-  for b in generate(defaultState, 8):
+  for b in generate(state, 8):
     bytes[i] = b
     inc i
   number = cast[uint64](bytes)
   number = (0x3FFu64 shl 52) or (number shr 12)
   cast[float64](number) - 1.0
+
+proc rand*(): float =
+  rand(defaultState)
