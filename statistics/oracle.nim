34dfa2b975b46b95b87ac28990a5eac135f6ac38
Jack VanDrunen <jack@jackv.co>
2018-06-16 21:49:45 -0700

    Moved to PCG for random generation

--- statistics/oracle.nim
+++ statistics/oracle.nim
@@ -1,34 +1,48 @@
-import ./private/arc4
-
-const Discard = 3072
+type
+  PCG32Random = ref object
+    state, stream: uint64
+  Oracle = ref object
+    s1, s2: PCG32Random
 
 var defaultState: Oracle
 
-proc initOracle*(seed: uint64): Oracle =
-  result = initARC4(cast[array[0..7, uint8]](seed))
-  for b in generate(result, Discard):
-    discard
+proc toUniform(a, b: uint32): float64 =
+  (float64(a shr 6) * 134217728.0 + float64(b shr 5)) / 9007199254740992.0
 
-when not defined(nimscript):
-  import times
+proc initOracle*(seed: array[0..3, uint64]): Oracle =
+  result = new Oracle
+  result.s1 = new PCG32Random
+  result.s1.state = seed[0]
+  result.s1.stream = seed[1] or 1'u64
+  result.s2 = new PCG32Random
+  result.s2.state = seed[2] 
+  result.s2.stream = seed[3] or 1'u64
 
-  proc initOracle*(): Oracle =
-    initOracle(uint64(epochTime() * 1_000_000_000))
+proc rand(rng: var PCG32Random): uint32 =
+  let oldstate = rng.state
+  rng.state = oldstate * 6364136223846793005'u64 + rng.stream
+  let xorshifted = uint32(((oldstate shr 18'u64) xor oldstate) shr 27'u64)
+  let rot = int(oldstate shr 59'u64)
+  (xorshifted shr rot) or (xorshifted shl ((-rot) and 31))
 
-  defaultState = initOracle()
-else:
-  {.error: "oracle is not supported for the nimscript target".}
+proc rand*(oracle: var Oracle): float64 =
+  toUniform(rand(oracle.s1), rand(oracle.s2))
 
-proc rand*(state: var Oracle): float =
-  var bytes: array[0..7, uint8]
-  var number: uint64
-  var i = 0
-  for b in generate(state, 8):
-    bytes[i] = b
-    inc i
-  number = cast[uint64](bytes)
-  number = (0x3FFu64 shl 52) or (number shr 12)
-  cast[float64](number) - 1.0
-
-proc rand*(): float =
+proc rand*(): float64 =
   rand(defaultState)
+
+when defined(nimscript):
+  proc initOracle*(): Oracle =
+    initOracle([0x853c49e6748fea9b'u64, 0xda3e39cb94b95bdb'u64, 0x9bea8f74e6493c85'u64, 0xdb5bb994cb393eda'u64])
+else:
+  import times
+  proc initOracle*(): Oracle =
+    result = new Oracle
+    result.s1 = new PCG32Random
+    result.s1.state = uint64(epochTime() * 1_000_000_000)
+    result.s1.stream = uint64(cast[int](addr result.s1)) or 1'u64
+    result.s2 = new PCG32Random
+    result.s2.state = uint64(epochTime() * 1_000_000_000)
+    result.s2.stream = uint64(cast[int](addr result.s2)) or 1'u64
+
+defaultState = initOracle()
--- tests/test_oracle.nim
+++ tests/test_oracle.nim
@@ -1,19 +1,19 @@
-import ../statistics/private/arc4
 import ../statistics/oracle
+import ./ut_utils
 import unittest
 
 suite "statistics-oracle":
-  let v1 = @[116u8, 148, 194, 231, 16, 75, 8, 121, 13, 75, 213, 83, 50, 143, 30, 252]
-  let v2 = @[219u8, 194, 74, 211, 97, 45, 11, 136, 238, 223, 113, 170, 94, 82, 11, 45]
-  var r1 = initARC4(@[0x01u8, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF])
-  var r2 = initARC4(@[0xFEu8, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10])
-
-  test "arc4":
-    check(r1.randbytes(16) == v1)
-    check(r2.randbytes(16) == v2)
+  var o = initOracle([0x853c49e6748fea9b'u64, 0xda3e39cb94b95bdb'u64, 0x9bea8f74e6493c85'u64, 0xdb5bb994cb393eda'u64])
 
   test "default state":
     let
       x = rand()
       y = rand()
     check(x != y)
+
+  test "seeded":
+    check(approx(o.rand(), 0.082713))
+    check(approx(o.rand(), 0.009710))
+    check(approx(o.rand(), 0.793087))
+    check(approx(o.rand(), 0.974792))
+    check(approx(o.rand(), 0.112663))
